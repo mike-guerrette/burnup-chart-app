fe657a1d09ace856df4e27582157e0270e8e5a98
henry.lee <henrylee423@gmail.com>
2014-04-01 16:01:39 -0400

    Added date validation for Task, added background color for all pages, added styling to task data table.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,16 @@
 class Task < ActiveRecord::Base
   belongs_to :project
   validates :tasktype, presence:true
+  validate :end_after_start
+
+  private
+  def end_after_start
+    if end_date < start_date
+      log = File.open("log.txt", 'w')
+      log.puts(end_date)
+      log.close()
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
+
 end
