ae63b3e4d22ebb941cf5ab2202e3e610fb75907f
mike-guerrette <mk.guerrette@gmail.com>
2014-03-31 20:38:44 -0400

    Changed paths for links.  Clicking on project name now links to tasks.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,8 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
+    @projects = Project.find (params[:id])
+    redirect_to project_tasks_path(@projects)
   end
 
   # GET /projects/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :projects
 
   get 'home/index'
 
@@ -7,7 +6,7 @@ Rails.application.routes.draw do
     resources :tasks
   end
 
-  root to: "home#index"
+  root to: "projects#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
