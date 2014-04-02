67c171f9487a3a1a524c15db1db980056155da38
henry.lee <henrylee423@gmail.com>
2014-04-02 16:55:33 -0400

    Task_time_table now shoes some data, gets number of tasks and days spent working on those tasks

--- app/controllers/task_time_table_controller.rb
+++ app/controllers/task_time_table_controller.rb
@@ -4,12 +4,28 @@ class TaskTimeTableController < ApplicationController
     @tasks = @project.tasks
     @tasktypes = @tasks.pluck('DISTINCT tasktype')
     @data = []
-    #@tasktypes.each do |ttype|
-    # tasks = @tasks.where("tasktype = #{ttype}")
-    #  tasks.each do |task|
-    #    @data.append(task.tasktype)
-    #  end
-    #end
+    #@data.append(@tasks.group(:tasktype).count)
+
+    @tasktypes.each do |ttype|
+      tempHash = Hash.new()
+      tempHash[:task_type] = ttype
+      tasks = @tasks.where(tasktype: ttype)
+      tempHash[:num_tasks] = tasks.length
+
+      totalDays = 0
+      tasks.each do |task|
+        if task.days_on_hold.nil?
+          days = (task.end_date - task.start_date).to_i
+        else
+          days = (task.end_date - task.start_date).to_i - task.days_on_hold
+        end
+        totalDays += days
+      end
+      tempHash[:num_days] = totalDays
+      @data.append(tempHash)
+
+    end
+
 
   end
 end
