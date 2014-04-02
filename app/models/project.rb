3e584d5ae4c3439eac327e5dddfd9cda045323a2
henry.lee <henrylee423@gmail.com>
2014-04-02 13:41:59 -0400

    Changed how URL is generated, now shows the name of project instead of project_id

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,18 @@
 class Project < ActiveRecord::Base
+
   has_many :tasks
   validates :name, presence:true
+  #validates_format_of :name, :without => /^\d/
+
+  def self.find(input)
+    if input.is_a?(Integer)
+      super
+    else
+      find_by_name(input)
+    end
+  end
+
+  def to_param
+    name
+  end
 end
