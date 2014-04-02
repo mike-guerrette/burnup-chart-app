bf6fb52af0e9900d9dda5bf11fd5d4a3bf923637
henry.lee <henrylee423@gmail.com>
2014-04-02 16:20:10 -0400

    Added task_time_table controller, created preliminary chart that can find unique task types, added link to task_time_table index page

--- app/controllers/task_time_table_controller.rb
+++ app/controllers/task_time_table_controller.rb
@@ -3,10 +3,13 @@ class TaskTimeTableController < ApplicationController
     @project = Project.find(params[:project_id])
     @tasks = @project.tasks
     @tasktypes = @tasks.pluck('DISTINCT tasktype')
-    data = []
-    @tasktypes.each do |ttype|
-      tasks = @tasks.where('tasktype == #{ttype}')
-    end
+    @data = []
+    #@tasktypes.each do |ttype|
+    # tasks = @tasks.where("tasktype = #{ttype}")
+    #  tasks.each do |task|
+    #    @data.append(task.tasktype)
+    #  end
+    #end
 
   end
 end
