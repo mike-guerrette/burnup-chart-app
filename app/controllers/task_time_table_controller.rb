cef0291ddc0252406cfc981b530672b093567807
henry.lee <henrylee423@gmail.com>
2014-04-03 09:43:47 -0400

    Restricted "Days on Hold" form field to only positive numbers and now defaults to 0.

--- app/controllers/task_time_table_controller.rb
+++ app/controllers/task_time_table_controller.rb
@@ -6,7 +6,6 @@ class TaskTimeTableController < ApplicationController
     @tasks = @project.tasks
     @tasktypes = @tasks.pluck('DISTINCT tasktype')
     @data = []
-    #@data.append(@tasks.group(:tasktype).count)
 
     @tasktypes.each do |ttype|
       tempHash = Hash.new()
@@ -15,20 +14,24 @@ class TaskTimeTableController < ApplicationController
       tempHash[:num_tasks] = tasks.length
 
       totalDays = 0
+      daysOnHold = 0
       tasks.each do |task|
         if task.days_on_hold.nil?
-          #days = (task.end_date - task.start_date).to_i
+          #Need to make sure this is getting the correct number of days when compared to Excel.
           days = task.start_date.business_days_until(task.end_date)
         else
           #days = (task.end_date - task.start_date).to_i - task.days_on_hold
+          daysOnHold += task.days_on_hold
           days = task.start_date.business_days_until(task.end_date) - task.days_on_hold
         end
         totalDays += days
       end
       tempHash[:num_days] = totalDays
+      tempHash[:days_on_hold] = daysOnHold
       @data.append(tempHash)
 
     end
+
 
 
   end
